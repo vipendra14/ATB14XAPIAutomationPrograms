787446c8130cbd60d75155ac0fda5c7ac1912968
Vipendra <vipendra.mishra000@gmail.com>
2025-12-29 20:45:05 +0530

    First API Program

--- src/test/java/com/APIRestAssuredPrograms/Hello_World.java
+++ src/test/java/com/APIRestAssuredPrograms/Hello_World.java
@@ -1,4 +1,13 @@
 package com.APIRestAssuredPrograms;
 
+import io.restassured.RestAssured;
+
 public class Hello_World {
+    public static void main(String[] args) {
+        RestAssured
+                .given().baseUri("https://www.google.com/").log().all()
+                .when().get()
+                .then().statusCode(200).log().all();
+
+    }
 }
